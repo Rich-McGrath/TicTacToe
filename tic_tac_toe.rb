08c18a3701824d887bc619c6ff4f7e8606218d1a
Rich-McGrath <richmcgrath@gmail.com>
2015-09-25 23:15:09 -0400

    Application requirements

---
+++ tic_tac_toe.rb
@@ -0,0 +1,5 @@
+# Tic Tac Toe
+# 1. Come up with requirments/specification, whihc will determine the scope.
+# 2. Application logic; sequence of steps
+# 3. Translation of those steps into code
+# 4. Run code to verify logic.
