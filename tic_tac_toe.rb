b24e463bebb790ff00c7dabc9c368c0b74016be9
Rich-McGrath <richmcgrath@gmail.com>
2015-09-25 23:32:02 -0400

    Psuedo code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,4 +2,22 @@
 # 1. Come up with requirments/specification, whihc will determine the scope.
 # 2. Application logic; sequence of steps
 # 3. Translation of those steps into code
-# 4. Run code to verify logic.
+# 4. Run code to verify logic
+#
+
+# Psuedo Code
+# draw a board
+#
+# assing player1 to "X"
+# assign player2 to "O"
+#
+# loop until a winner or all squares are taken
+#   player1 picks an empty square
+#   check for winner
+#   player2 picks an empty square
+#   check for winner
+#
+# if there's a winner
+#   show the winner
+# or else
+#   it's a tie
