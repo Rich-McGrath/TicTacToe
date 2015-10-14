fac2423871df0cc173ee486444ec33c1afe155d7
Rich-McGrath <richmcgrath@gmail.com>
2015-10-14 00:06:18 -0400

    Broke Winning Lines out from its method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -50,14 +50,25 @@ def player_picks_square(b)
   b[position] = 'X'
 end
 
+#def two_in_a_row(check_winner, board)
+#    if board.values_at()
+#end
+
 def computer_picks_square(b)
   position = empty_positions(b).sample
   b[position] = 'O'
 end
 
-def check_winner(board)
-  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+#def winning_combinations
+#  winning_lines = {}
+winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+#  winning_lines
+#end
+
+def check_winner(board, winning_lines)
+  #winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
   winning_lines.each do |line|
+  #win.each do |line|
     return 'Player' if board.values_at(*line).count('X') == 3
     return 'Computer' if board.values_at(*line).count('0') == 3
   end
@@ -68,6 +79,11 @@ def announce_winner(winner)
   puts "#{winner} won!"
 end
 
+def nine_positions_are_filled?(board)
+  empty_positions(board) == []
+end
+
+  #win = winning_combinations
   board = initialize_board
   draw_board(board)
 
@@ -75,8 +91,8 @@ begin
   player_picks_square(board)
   computer_picks_square(board)
   draw_board(board)
-  winner = check_winner(board)
-end until winner || empty_positions(board).empty?
+  winner = check_winner(board, winning_lines)
+end until winner || nine_positions_are_filled?(board)
 
 if winner
   announce_winner(winner)
